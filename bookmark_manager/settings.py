8fc940032c6244a6858cd1d426a92bd4c8e01520
Clyde Charles <clydecc09@gmail.com>
2025-10-08 14:36:29 +0530

    Update settings.py

--- bookmark_manager/settings.py
+++ bookmark_manager/settings.py
@@ -98,6 +98,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MaximumPasswordValidator',
+    },
 ]
 
 
